28a1ef1e242fd471f5fb0a8442794a7c9e39efc8
dong417 <445709797@qq.com>
2019-03-11 23:11:54 +0800

    李贤东

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    /**
+    *  @author: lixiandong
+    *  @Date: 2019/3/11 23:08
+    *  @Description:
+    */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
